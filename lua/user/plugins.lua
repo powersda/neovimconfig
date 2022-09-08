0565addddb15e65c4a5b4e7a99bed5d107d6883b
David Powers <34976872+powersda@users.noreply.github.com>
2022-09-08 01:25:15 -0400

    Added flutter tools, dartls config

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -12,12 +12,12 @@ if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
 end
 
 -- Autocommand that reloads neovim whenever you save the plugins.lua file
-vim.cmd([[
-    augroup packer_user_config
-        autocmd!
-        autocmd BufWritePost plugins.lua source <afile> | PackerSync
-    augroup end
-]])
+-- vim.cmd([[
+--     augroup packer_user_config
+--         autocmd!
+--         autocmd BufWritePost plugins.lua source <afile> | PackerSync
+--     augroup end
+-- ]])
 
 -- Use a protected call so we don't error out on first use
 local status_ok, packer = pcall(require, "packer")
@@ -70,6 +70,8 @@ return packer.startup(function(use)
 	use({ "neovim/nvim-lspconfig" })
     use({ "williamboman/mason.nvim" })
     use({ "williamboman/mason-lspconfig.nvim" })
+    use {'akinsho/flutter-tools.nvim', requires = 'nvim-lua/plenary.nvim'}
+
 
 	-- Treesitter
 	use({ "nvim-treesitter/nvim-treesitter", run = function() require('nvim-treesitter.install').update({ with_sync = true }) end })
